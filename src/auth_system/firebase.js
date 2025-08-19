41a8dd08670960acc8d85b84f8857441dfb8ff68
Mukesh V <vmukeshofficial@gmail.com>
2025-08-20 03:28:27 +0530

    Update firebase.js

--- src/auth_system/firebase.js
+++ src/auth_system/firebase.js
@@ -7,12 +7,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
-  apiKey: "Your apiKey",
-  authDomain: "Your authDomain",
-  projectId: "Your projectId",
-  storageBucket: "Your storageBucket",
-  messagingSenderId: "Your messagingSenderId",
-  appId: "Your appId"
+  apiKey: "AIzaSyDJwMjg9mQIWocWBb_66yykCDWGzxnswOw",
+
+authDomain: "1ms-dashboard-6584e.firebaseapp.com",
+
+projectId: "1ms-dashboard-6584e",
+
+storageBucket: "1ms-dashboard-6584e.appspot.com",
+
+messagingSenderId: "864176192600",
+
+appId: "1:864176192600:web:4c518321d44c75f6d51e2d"
 };
 
 const app = initializeApp(firebaseConfig);
